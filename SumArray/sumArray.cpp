ea34911633d5e411423fe31809295e6204cae061
Willian Chu <willianchu@live.com>
2022-05-30 12:27:20 -0700

    sizeof()

--- SumArray/sumArray.cpp
+++ SumArray/sumArray.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main (){
    int arr[] = { 2, 5, 7, 8, 2, 6, 9 };
-   int n = 7;
+   int size = sizeof(arr)/sizeof(arr[0]);
+   cout << "Size of array is " << size << endl;
    int sum = 0;
-   for(int i = 0; i<n ; i++){
+   for(int i = 0; i< size ; i++){
       sum+=arr[i];
    }
    cout << "The array sum is " << sum << endl;
