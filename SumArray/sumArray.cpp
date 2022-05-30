60bcd1ed57c31287a006b09d6bb6101ec0951b1e
Willian Chu <willianchu@live.com>
2022-05-30 12:40:31 -0700

    size with pointer *(&arr+1)-arr

--- SumArray/sumArray.cpp
+++ SumArray/sumArray.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 int main (){
-   int arr[] = { 2, 5, 7, 8, 2, 6, 9 };
-   int size = sizeof(arr)/sizeof(arr[0]);
+   int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+   int len = *(&arr + 1) - arr;
+   int size = sizeof(arr) / sizeof(arr[0]);
    cout << "Size of array is " << size << endl;
+   cout << "Len of array is " << len << endl;
    int sum = 0;
    for(int i = 0; i< size ; i++){
       sum+=arr[i];
