91364b9696846bea27e25c484d9d7e2df5366a42
Willian Chu <willianchu@hotmail.com>
2022-06-09 10:18:19 -0700

    with stream in comments

--- stringstream/stream.cpp
+++ stringstream/stream.cpp
@@ -31,3 +31,14 @@ int main() {
 
     return 0;
 }
+
+// vector<int> parseInts(string str) {
+// stringstream ss(str);   //??
+// vector<int> result;
+// char ch;
+// int tmp;
+// while(ss >> tmp) {      //??
+//     result.push_back(tmp);
+//     ss >> ch;           //??
+// }
+// return result;
