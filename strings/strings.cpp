3e63ce1965f20dc31a090cd1d437e448f10ff66d
Willian Chu <willianchu@hotmail.com>
2022-06-09 11:24:21 -0700

    could be use swap and length()

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -16,3 +16,22 @@ int main() {
 
     return 0;
 }
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// int main()
+// {
+//     string a, b;
+//     cin >> a >> b;
+
+//     cout << a.length() << ' ' << b.length() << endl;
+
+//     cout << a + b << endl;
+
+//     swap( a[0], b[0] );  // could use swap( a[0], b[0] )
+//     cout << a << ' ' <<  b << endl;
+
+//     return 0;
+// }
