f7080730abd80c6f0911c6eddd7bae0cbb1e0bbb
Willian Chu <willianchu@hotmail.com>
2022-06-09 12:10:38 -0700

    setter and getter

---
+++ class/class.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Student {
+    private:
+    int age, standart;
+    string first_name, last_name;
+    public:
+    void set_age (int a){
+        age = a;
+    }
+    int get_age() {
+        return age;
+    }
+    void set_standart (int a){
+        standart = a;
+    }
+    int get_standart() {
+        return standart;
+    }
+    void set_first_name (string a){
+        first_name = a;
+    }
+    string get_first_name() {
+        return first_name;
+    }
+    void set_last_name (string a){
+        first_name = a;
+    }
+    string get_last_name() {
+        return first_name;
+    }
+};
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+
+    cin >> age >> first_name >> last_name >> standard;
+
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+
+    return 0;
+}
