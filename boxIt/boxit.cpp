d1e24d6ef7f2f004404a319f832158f08b3649f9
Willian Chu <willianchu@hotmail.com>
2022-06-10 10:46:02 -0700

    operator << overloading

--- boxIt/boxit.cpp
+++ boxIt/boxit.cpp
@@ -37,9 +37,11 @@ public:
     long long CalculateVolume(){
         return (long long)l * b * h;
     }
+};
+
 //Overload operator < as specified
 //bool operator<(Box& b)
-    bool Box operator<(Box& B){
+    bool operator<(Box& A, Box& B){
         if( A.CalculateVolume() < B.CalculateVolume()) {
             return true;
         } else {
@@ -49,4 +51,58 @@ public:
 
 //Overload operator << as specified
 //ostream& operator<<(ostream& out, Box& B)
-};
+    void operator<<(ostream& COUT, Box& B){
+        COUT<<B.getLength()<<" "<<B.getBreadth()<<" "<<B.getHeight();
+    };
+void check2()
+{
+	int n;
+	cin>>n;
+	Box temp;
+	for(int i=0;i<n;i++)
+	{
+		int type;
+		cin>>type;
+		if(type ==1)
+		{
+			cout<<temp<<endl;
+		}
+		if(type == 2)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			temp=NewBox;
+			cout<<temp<<endl;
+		}
+		if(type==3)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			if(NewBox<temp)
+			{
+				cout<<"Lesser\n";
+			}
+			else
+			{
+				cout<<"Greater\n";
+			}
+		}
+		if(type==4)
+		{
+			cout<<temp.CalculateVolume()<<endl;
+		}
+		if(type==5)
+		{
+			Box NewBox(temp);
+			cout<<NewBox<<endl;
+		}
+
+	}
+}
+
+int main()
+{
+	check2();
+}
