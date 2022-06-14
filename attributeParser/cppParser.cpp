77c8aaf24d99603625d17afa3998bef06c4caa04
Willian Chu <willianchu@hotmail.com>
2022-06-14 14:00:45 -0700

    test included

--- attributeParser/cppParser.cpp
+++ attributeParser/cppParser.cpp
@@ -9,12 +9,41 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-  int N, Q; cin >> N >> Q;
+  int N, Q;
+  N = 12;
+  Q = 8;
+  vector<int> num {1, 2, 3, 4, 5};
+  vector<string> A {
+  '    <tag1 value = "HelloWorld">',
+  '	<tag2 name = "Name1">',
+  '		<tag3 name = "Name3">',
+  '		</tag3>',
+  '		<tag4 name = "Name4" text = "Super">',
+  '			<tag6 buzz = "Buzz6">',
+  '			</tag6>',
+  '		</tag4>',
+  '		<tag5>',
+  '		</tag5>',
+  '	</tag2>',
+  '</tag1>'
+  };
+  vector<string> B(N) = {
+    "tag1.tag2~name",
+    "tag1~name",
+    "tag1~value",
+    "tag1.tag2.tag3~name",
+    "tag1.tag2.tag3~value",
+    "tag1.tag2.tag4~text",
+    "tag1.tag2.tag4.tag6~text",
+    "tag1.tag2.tag4.tag6~buzz"
+  }
+  // cin >> N >> Q;
   vector<string> stack;
   string tag, element,value;
   map<string, string> tree;
   for(int i = 0; i < N + 1; i++) {
-    string entireLine; getline(cin, entireLine);
+    // string entireLine; getline(cin, entireLine);
+    string entireLine = A[i];
     int numOfCharacters = entireLine.length();
     int getElement = 0;
     for (int j = 0; j < numOfCharacters; j++) {
@@ -79,14 +108,15 @@ int main() {
     }
 
   }
-//   cout << "tree: " << endl;
+  cout << "tree: " << endl;
   // print the tree
-  // for( map<string,string>::iterator i=tree.begin(); i!=tree.end(); ++i)
-  //   {
-  //   cout << (*i).first <<  " = " << (*i).second << endl;
-  //   }
+  for( map<string,string>::iterator i=tree.begin(); i!=tree.end(); ++i)
+    {
+    cout << (*i).first <<  " = " << (*i).second << endl;
+    }
   for(int i = 0; i < Q; i++) {
-    string query; getline(cin, query);
+    // string query; getline(cin, query);
+    string query = B[i];
     if(tree[query] == "") {
       cout << "Not Found!" << endl;
     } else {
