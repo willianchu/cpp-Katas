e624ce735eb9688cb04fda90b83cbfe6c8300aa0
Willian Chu <willianchu@hotmail.com>
2022-06-16 13:45:11 -0700

    upper_bound/lower bound)

--- lowerBound/bound.cpp
+++ lowerBound/bound.cpp
@@ -5,22 +5,17 @@
 #include <algorithm>
 using namespace std;
 
-bool isPresent(vector<int> &v, int n) {
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] == n) {
-            return true;
-        }
-    }
-    return false;
-}
 int main() {
     int N; cin >> N;
     vector<int> arr(N); for (int i = 0; i < N; i++) cin >> arr[i];
     int Q; cin >> Q;
     for (int i = 0; i < Q; i++) {
         int value; cin >> value;
-        bool answer = isPresent(arr, value);
+        int low = lower_bound(arr.begin(), arr.end(), value) - arr.begin();
+        int high = upper_bound(arr.begin(), arr.end(), value) - arr.begin();
+        string answer = low == high ? "No " : "Yes ";
+        cout << answer << low + 1 << endl;
     }
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
     return 0;
 }
