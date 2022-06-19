78453c385070c2772838a1abee8a1cd7a4c02502
Willian Chu <willianchu@hotmail.com>
2022-06-19 11:51:01 -0700

    setw, showbase, left, right, internal, hex, scientific, setprecision

--- printPretty/pretty.cpp
+++ printPretty/pretty.cpp
@@ -12,16 +12,43 @@ int main() {
 		double C; cin >> C;
 
 
-    cout << "0x" << hex << (int)A << endl;
-    //B: Print it to a scale of  decimal places, preceded by a  or  sign (indicating if it's positive or negative), right justified, and left-padded with underscores so that the printed result is exactly  characters wide.
-    string space_signal = (B < 0) ? "-" : "+";
-    cout << space_signal << setprecision(2) << fixed << B << endl;
+        cout << hex << left << showbase << nouppercase; // formatting
+        cout << (long long) A << endl; // actual printed part
 
+        // LINE 2
+        cout << dec << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2); // formatting
+        cout << B << endl; // actual printed part
 
-    cout << scientific << setprecision(9) << C << endl;
+        // LINE 3
+        cout << scientific << uppercase << noshowpos << setprecision(9); // formatting
+        cout << C << endl; // actual printed part
 
 
 	}
 	return 0;
 
 }
+
+// printf("%#lx\n",(long int)A);
+// printf("%.*s%+.2f\n",15 - to_string((int)B).length() - 4,"_______________",B);
+// printf("%.9E\n",C);
+
+// ios_base::fmtflags flags = cout.flags();
+// cout << setw(0) << "0x" << hex << nouppercase << left << (long long)A << endl;
+// cout.flags(flags);
+// cout << fixed << setw(15) << setprecision(2) << setfill('_') << right << showpos << B << endl;
+// cout.flags(flags);
+// cout << scientific << setw(15) << setprecision(9) << noshowbase << right << C << endl;
+// cout.flags(flags);
+
+// LINE 1
+        cout << hex << left << showbase << nouppercase; // formatting
+        cout << (long long) A << endl; // actual printed part
+
+        // LINE 2
+        cout << dec << right << setw(15) << setfill('_') << showpos << fixed << setprecision(2); // formatting
+        cout << B << endl; // actual printed part
+
+        // LINE 3
+        cout << scientific << uppercase << noshowpos << setprecision(9); // formatting
+        cout << C << endl; // actual printed part
