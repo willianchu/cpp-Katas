ee8b36125359f9b9c9bf496e821efcc958585154
Willian Chu <willianchu@hotmail.com>
2022-06-19 10:54:06 -0700

    set enviorment

--- sets/sets.cpp
+++ sets/sets.cpp
@@ -8,7 +8,28 @@ using namespace std;
 
 
 int main() {
+    int Q; cin >> Q; // number of queries
+    set<int> setOfNumbers;
+    while (Q--) {
+        int operation, number; cin >> operation >> number;
+        if (operation == 1) {
+            setOfNumbers.insert(number); // insert number into set
+        } else if (operation == 2) {
+            setOfNumbers.erase(number); // erase number from set
+        } else if (operation == 3) {
+            if (setOfNumbers.find(number) != setOfNumbers.end()){
+                cout << "Yes" << endl;
+            } else {
+                cout << "No" << endl;
+            }; // check if number is in set
+
+        }
+    }
+
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
 }
+
+
+
 
