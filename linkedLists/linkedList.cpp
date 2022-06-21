7a2b4cc46a577d9c238d1968094e15246efe030f
Willian Chu <willianchu@hotmail.com>
2022-06-21 13:01:02 -0700

    simple link

--- linkedLists/linkedList.cpp
+++ linkedLists/linkedList.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class Node {
+public:
+  int Value;
+  Node* Next;
+
+};
+
+void printList(Node*n) {
+  while(n!=NULL) {
+    cout << n->Value << endl;
+    n=n->Next;
+  }
+}
+
 int main()
 {
+  Node* head = new Node(); //convention standard
+  Node* second = new Node();
+  Node* third = new Node();
+
+  head->Value = 1;
+  head->Next = second;
+  second->Value = 2;
+  second->Next = third;
+  third->Value = 3;
+  third->Next = NULL;
+
+  printList(head);
+
+
 
   system("pause>0");
 }
