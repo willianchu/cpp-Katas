44f6e92307442438de8eab1ead28545f18bff3dc
Willian Chu <willianchu@hotmail.com>
2022-06-22 13:09:21 -0700

    auto pair : dictionary

--- map/map.cpp
+++ map/map.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <map>
+#include <string>
 
+using namespace std;
 int main()
 {
+  map<string, string> cDictionary;
+  cDictionary.insert(pair<string, string>("maverick", "Tom Cruise"));
+  cDictionary.insert(pair<string, string>("titanic", "Leonardo Di Caprio"));
+  cDictionary.insert(pair<string, string>("back to the future", "Michael J. Fox"));
+  cDictionary.insert(pair<string, string>("matrix", "Keanu Reeves"));
 
+  for(auto pair : cDictionary) {
+    cout << pair.first << " - " << pair.second << endl;
+  }
   system("pause>0");
 }
