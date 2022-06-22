0e6f23df3c175693f1cef1499e8e72f60cb20f85
Willian Chu <willianchu@hotmail.com>
2022-06-22 13:48:03 -0700

    map of list nested for auto

--- map/map.cpp
+++ map/map.cpp
@@ -2,22 +2,44 @@
 #include <map>
 // #include <unordered_map>
 #include <string>
+#include <list>
 
 using namespace std;
 int main()
 {
-  map<string, string> cDictionary;
-  // unordered_map<string, string> cDictionary; // preserve order in LIFO
-  cDictionary.insert(pair<string, string>("maverick", "Tom Cruise"));
-  cDictionary.insert(pair<string, string>("titanic", "Leonardo Di Caprio"));
-  cDictionary.insert(pair<string, string>("back to the future", "Michael J. Fox"));
-  cDictionary.insert(pair<string, string>("matrix", "Keanu Reeves"));
+  // complex dictionary
+  map<string, list<string>> airDex;
 
-  cDictionary["maverick"] = "Jennifer Gardener";
-  // cDictionary.clear(); cDictionary.erase("maverick");
-  cout << cDictionary.size()<<endl;
-  for(auto pair : cDictionary) {
-    cout << pair.first << " - " << pair.second << endl;
-  }
+  list<string>
+    tomCat{"two engines", "variable wings", "two tails", "single radar"},
+    hornet{"two engines", "fixed wings", "two tails", "4 radars"},
+    tiger{"one engine", "fixed wings", "one tail", "two radars"  };
+
+    airDex.insert(pair<string, list<string>>("F14", tomCat));
+    airDex.insert(pair<string, list<string>>("F18", hornet));
+    airDex.insert(pair<string, list<string>>("F5E", tiger));
+
+    for(auto pair : airDex){
+      cout << pair.first << " - ";
+
+      for(auto spec : pair.second){
+        cout << spec <<", ";
+      }
+      cout << endl;
+    }
+
+  // map<string, string> cDictionary;
+  // // unordered_map<string, string> cDictionary; // preserve order in LIFO
+  // cDictionary.insert(pair<string, string>("maverick", "Tom Cruise"));
+  // cDictionary.insert(pair<string, string>("titanic", "Leonardo Di Caprio"));
+  // cDictionary.insert(pair<string, string>("back to the future", "Michael J. Fox"));
+  // cDictionary.insert(pair<string, string>("matrix", "Keanu Reeves"));
+
+  // cDictionary["maverick"] = "Jennifer Gardener";
+  // // cDictionary.clear(); cDictionary.erase("maverick");
+  // cout << cDictionary.size()<<endl;
+  // for(auto pair : cDictionary) {
+  //   cout << pair.first << " - " << pair.second << endl;
+  // }
   system("pause>0");
 }
