ba261b5cea0f2e08c92059d726772920d6b67dee
Willian Chu <willianchu@hotmail.com>
2022-07-05 16:46:43 -0700

    problem structure

---
+++ Workshops/workshops.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Define the structs Workshops and Available_Workshops.
+//Implement the functions initialize and CalculateMaxWorkshops
+
+int main(int argc, char *argv[]) {
